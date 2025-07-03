c826d23071e3491ae708df430be86188fda690bd
Hue Henry <mhenry@uat.edu>
2025-07-02 17:09:28 -0700

    Updated to Chicken Banana

---
+++ Assets/Code/HelloWorld.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Awake runs when the object is created, but just before it enters the world (before Start)
+    void Awake()
+    {
+        
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Chicken Banana!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
